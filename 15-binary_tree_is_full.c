9d5a8e036d96f5f641b20c286a8da56c68182946
Chloexlsx <chloe850350@gmail.com>
2025-08-12 06:27:24 +0000

    task_15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -14,6 +14,8 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (tree->left != NULL && tree->right != NULL)
 		return (1);
 	if (tree->left == NULL || tree->right == NULL)
+		if(tree->parent == NULL)
+			return (1);
 		return (0);
 	return (binary_tree_is_full(tree->left)
 && binary_tree_is_full(tree->right));
